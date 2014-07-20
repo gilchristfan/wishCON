865a0fd6545011e3492df7f01d36cf992d8c7b9b
Arun Dutta <gilchristfan@gmail.com>
2014-07-20 22:41:19 +0530

    Update ChangePwd.java

--- src/org/saJava/GUI/ChangePwd.java
+++ src/org/saJava/GUI/ChangePwd.java
@@ -7,13 +7,7 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.io.IOException;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Goldberg
- * Date: 3/7/13
- * Time: 8:55 PM
- * To change this template use File | Settings | File Templates.
- */
+
 public class ChangePwd extends JDialog {
 
     /**
